ca9a1c9a4c9cb0b3b8c479811646d4878c6d05db
milan-stankovic-dev <milance.za019@gmail.com>
2024-03-10 17:02:02 +0100

    Added test cases for views.component

--- src/app/person/views/views.component.spec.ts
+++ src/app/person/views/views.component.spec.ts
@@ -1,21 +1,55 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ViewsComponent } from './views.component';
+import { PersonViewService } from 'src/app/service/person-view.service';
+import { DebugElement } from '@angular/core';
+import { PersonTableComponent } from '../person-table/person-table.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PersonDisplay } from 'src/app/domain/PersonDisplay';
+import { ADULTS, SMEDEREVCI } from 'src/app/test-data/persons';
+import { of } from 'rxjs/internal/observable/of';
+import { By } from '@angular/platform-browser';
 
-describe('ViewsComponent', () => {
+fdescribe('ViewsComponent', () => {
   let component: ViewsComponent;
   let fixture: ComponentFixture<ViewsComponent>;
+  let personViewServiceSpy: jasmine.SpyObj<PersonViewService>;
+  let el: DebugElement;
 
   beforeEach(() => {
+    personViewServiceSpy = jasmine.createSpyObj('PersonViewService',
+    ['getAllSmederevci', 'getAllAdults']);
+    personViewServiceSpy.getAllSmederevci.and.returnValue(of(SMEDEREVCI));
+    personViewServiceSpy.getAllAdults.and.returnValue(of(ADULTS));
+
     TestBed.configureTestingModule({
-      declarations: [ViewsComponent]
+      imports: [HttpClientTestingModule],
+      declarations: [ViewsComponent, PersonTableComponent],
+      providers: [{provide: PersonViewService, 
+        useValue: personViewServiceSpy}]
     });
+
     fixture = TestBed.createComponent(ViewsComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    el = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should fetch api data and display views properly.', ()=> {
+      fixture.detectChanges();
+
+      fixture.whenStable().then(()=>{
+        expect(component.smederevci).toBe(SMEDEREVCI);
+        expect(component.adults).toBe(ADULTS);
+
+        const smederevciTable = el.query(By.css('#smederevciTable'));
+        const adultsTable = el.query(By.css('#adultsTable'));
+
+        expect(smederevciTable).toBeTruthy();
+        expect(adultsTable).toBeTruthy();
+      });
   });
 });
