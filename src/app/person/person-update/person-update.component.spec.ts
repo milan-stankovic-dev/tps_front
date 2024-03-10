ef85d511534b35b9af0baf561394efe864ba2d82
milan-stankovic-dev <milance.za019@gmail.com>
2024-03-10 15:19:12 +0100

    Added tests for person-update.component.ts.

--- src/app/person/person-update/person-update.component.spec.ts
+++ src/app/person/person-update/person-update.component.spec.ts
@@ -1,21 +1,40 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { PersonUpdateComponent } from './person-update.component';
+import { PersonDropdownComponent } from '../person-dropdown/person-dropdown.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { PersonService } from 'src/app/service/person.service';
+import { FormsModule } from '@angular/forms';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-describe('PersonUpdateComponent', () => {
+fdescribe('PersonUpdateComponent', () => {
   let component: PersonUpdateComponent;
   let fixture: ComponentFixture<PersonUpdateComponent>;
+  let el: DebugElement;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      declarations: [PersonUpdateComponent]
+      declarations: [PersonUpdateComponent, PersonDropdownComponent],
+      imports: [HttpClientTestingModule, FormsModule]
     });
+
     fixture = TestBed.createComponent(PersonUpdateComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    el = fixture.debugElement;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should have app-person-dropdown with mode "ažuriranje"', () => {
+    const dropdownEl = el.queryAll(By.directive(
+          PersonDropdownComponent))[0];
+    expect(dropdownEl).toBeTruthy();
+    
+    const dropdownComponent = dropdownEl.componentInstance as PersonDropdownComponent;
+    expect(dropdownComponent.mode).toEqual('ažuriranje');
   });
 });
